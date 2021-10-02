d0f296e202b705030b70f14e649fe706ea356147
Madi-S <khovansky99@gmail.com>
2021-10-02 20:12:23 +0600

    Security enhancements

--- crypto.py
+++ crypto.py
@@ -1,18 +1,22 @@
-from secrets import token_urlsafe
-from cryptography.fernet import Fernet
+# from secrets import token_urlsafe
+# from cryptography.fernet import Fernet
 
 
-f = Fernet(Fernet.generate_key())
+# f = Fernet(Fernet.generate_key())
+
+from uuid import uuid4
 
 
 # binary key -> mandatory field for registration to validate for genuinity of volunteers and organizations
 def generate_key():
-    return f.generate_key()
+    return str(uuid4().hex + uuid4().hex[:12]).encode()
+    # return f.generate_key()
 
 
 # slug -> link to profile or whatever
 def generate_slug():
-    return token_urlsafe(16)
+    return uuid4().hex[:22]
+    # return token_urlsafe(16)
 
 
 if __name__ == '__main__':
